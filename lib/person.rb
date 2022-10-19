053eb544058dc61c1779c548f6d6c4120e36a80f
calvin <ncalvin67@gmail.com>
2022-10-19 11:31:07 +0300

    commit tree 8531321542a1e4344875946a68827a6f4b1afbf9

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
+    def name=(your_name)
+        @name = your_name
+    end
+    def name
+        @name
+    end
 
+    def job=(occupation)
+        @job = occupation
+    end
+    def job
+        @job
+    end
 end
